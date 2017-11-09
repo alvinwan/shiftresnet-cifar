e468cfe330ca6af39f98d777e1fb29f14c06b175
Bichen Wu <bichen@berkeley.edu>
2017-11-09 15:38:45 -0800

    Bichen: fixed downsampling In resent models.
    Accuracy number is comparable (better) than https://arxiv.org/pdf/1512.03385.pdf

--- models/resnet.py
+++ models/resnet.py
@@ -105,7 +105,7 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=2)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
         self.linear = nn.Linear(64, num_classes)
