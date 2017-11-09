26009b8eb7fbaaaaa28912e703313e536b743979
Bichen Wu <bichen@berkeley.edu>
2017-11-09 15:56:39 -0800

    Bichen: Fixed avg_pool dimension.

--- models/resnet.py
+++ models/resnet.py
@@ -123,7 +123,7 @@ class ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
